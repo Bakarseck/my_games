7882e03d7aee766a076e250adc4799afa1716160
bakseck <seck.bakar@ugb.edu.sn>
2023-06-11 00:32:06 +0000

    hangman

---
+++ hangman/game.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"os/exec"
+)
+
+
+
+func checkError(e error) {
+	if e != nil {
+		fmt.Println("We Encountered an error")
+		os.Exit(0)
+	}
+}
+
+func toFind(word string) int {
+	compteur := 0
+	for i:=0; i < len(word); i++ {
+		compteur++
+		for j:=i+1; j < len(word); j++ {
+			if word[i] == word[j] {
+				compteur--
+				break
+			}
+		}
+	}
+	return compteur
+}
+
+func Contains(c rune, word string) bool {
+	for _, v := range word {
+		if c == v {
+			return true
+		}
+	}
+	return false
+}
+
+func PrintWord(word, finded string) {
+	for _, c := range word {
+		if Contains(c, finded) {
+			fmt.Printf("%v ", string(c))
+		} else {
+			fmt.Print("_ ")
+		}
+	}
+}
+
+func Hangman(n int) {
+	switch(n) {
+	case 1:
+		fmt.Println(h1)
+	case 2:
+		fmt.Println(h2)
+	case 3:
+		fmt.Println(h3)
+	case 4:
+		fmt.Println(h4)
+	case 5:
+		fmt.Println(h5)
+	case 6:
+		fmt.Println(h6)
+	case 7:
+		fmt.Println(h7)
+	case 8:
+		fmt.Println(h8)
+	case 9:
+		fmt.Println(h9)	
+	}
+}
+
+var h1 = `
+ o`
+
+var h2 = `
+ o
+ |`
+
+var h3 = `
+ o
+ |
+ |`
+
+var h4 = `
+ o
+ |
+/|`
+
+var h5 = `
+ o
+ |
+/|\`
+
+var h6 = `
+ o
+ |
+/|\
+ |`
+
+var h7 = `
+ o
+ |
+/|\
+ |
+ |`
+
+var h8 = `
+ o
+ |
+/|\
+ |
+/|`
+
+var h9 = `
+ o
+ |
+/|\
+ |
+/|\`
---
+++ hangman/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"math/rand"
+	"strings"
+)
+
+func main() {
+	sample, err := os.ReadFile("./test.txt")
+
+	rand.Seed(time.Now().UnixNano())
+
+	checkError(err)
+
+	allWords := strings.Split(string(sample), "\n")
+
+	index := rand.Intn(len(allWords))
+
+	_word := allWords[index]
+
+	temp := strings.Split(_word, ": ")
+
+	word := temp[0]
+
+	n := len(word)
+
+	definition := temp[1]
+
+	compteur := 0
+	clearConsole("Welcome in our game of Hangman")
+
+	fmt.Println(strings.Repeat("_ ", n))
+	fmt.Println(definition)
+
+	letter := ""
+	finded := ""
+
+	find := toFind(word)
+
+	failed := 0
+
+	for failed != 9 {
+		fmt.Println()
+		fmt.Scanf("%v", &letter)
+		if len(letter) != 1 {
+			clearConsole(definition)
+			fmt.Println("You are only allowed to enter one character")
+			failed++
+			Hangman(failed)
+			PrintWord(word, finded)
+		} else {
+			if Contains(rune(letter[0]), word) {
+				finded += letter
+				if len(finded) == find {
+					clearConsole(definition)
+					PrintWord(word, finded)
+					fmt.Printf("\nCongratulations, you are finded the right answer in %v tries\n", compteur+1)
+					break
+				}
+				clearConsole(definition)
+				PrintWord(word, finded)
+			} else {
+				clearConsole(definition)
+				fmt.Println("Try again")
+				failed++
+				Hangman(failed)
+				PrintWord(word, finded)
+			}
+		}
+		compteur++
+	}
+}
