9c63d3c4fb95c1f9625e5c7986e84d5670b99d82
bakseck <seck.bakar.ugb.edu.sn>
2023-10-16 10:24:06 +0000

    add: kill

--- yote/index.js
+++ yote/index.js
@@ -1,4 +1,4 @@
-class YoteGame {
+class ChessGame {
     constructor() {
         this.pionToMove = null;
         this.isOrange = true;
@@ -17,6 +17,7 @@ class YoteGame {
         const allChilds = gridContainer.children;
         Object.entries(allChilds).map(([_, value]) => {
             value.addEventListener('click', (event) => {
+                
                 let positions = value.classList[1].split('-')[1];
 
                 if (value.childNodes.length == 0) {
@@ -25,10 +26,18 @@ class YoteGame {
                     } else {
 
                         let positionToMove = this.pionToMove.classList[1].split('-')[1];
-                        const validOffsets = [-1, 1, -5, 5];
+                        const validOffsets = [-1, 1, -5, 5 ];
+                        const KillAdvers = [-2, 2 , -10, 10]
 
                         if (validOffsets.some(offset => positions - positionToMove === offset)) {
                             this.movePion(this.pionToMove, value);
+                        } else if (KillAdvers.some(offset => positions - positionToMove === offset)) {
+                            let adversePosition = (Number(positions) + Number(positionToMove))/2
+
+                            if(this.isAdverse(adversePosition)) {
+                                this.movePion(this.pionToMove, value);
+                            }
+                            
                         }
 
                     }
@@ -41,15 +50,32 @@ class YoteGame {
                         value.style.background = "";
                     }
                 }
+                console.log(this.isOrange);
             });
         });
     }
+    isAdverse(advPos) {
+        let adverse = document.querySelector(`.case-${advPos}`)
+        if (adverse.childNodes.length == 1 && adverse.classList[2] != this.pionToMove.classList[2] ) {
+            let advPion = adverse.querySelector('img')
+            advPion.remove()
 
+            let advPlayer = adverse.classList[2]
+            adverse.classList.remove(advPlayer)
+
+            return true
+        }
+        return false
+    }
     movePion(toMove, destination) {
         let pion = toMove.querySelector("img");
         destination.append(pion);
         this.pionToMove.style.background = "";
         this.pionToMove = null;
+
+        let currPlayer = toMove.classList[2]
+        toMove.classList.remove(currPlayer)
+        destination.classList.add(currPlayer)
     }
 
     createDiv(i) {
@@ -85,6 +111,7 @@ class YoteGame {
     drawPions(currentPosition) {
         const img = document.createElement('img');
         const parent = document.querySelector('.case-' + currentPosition);
+        parent.classList.add(`${this.isOrange}`)
         img.style.width = '80%';
         img.style.height = '80%';
         img.style.borderRadius = '50%';
@@ -119,5 +146,5 @@ class YoteGame {
     `;
 }
 
-const yoteGame = new YoteGame();
-yoteGame.start();
+const chessGame = new ChessGame();
+chessGame.start();
